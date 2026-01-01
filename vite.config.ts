97e8b0b8b4b0b9244cb1f3d74e84c483cc8fd9f7
shoai <shoai@hostel.com>
2026-01-01 09:18:57 +0530

    Fix build config and renamed entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -12,7 +11,7 @@ export default defineConfig(({ mode }) => {
     build: {
       rollupOptions: {
         input: {
-          main: path.resolve('entry.html'),
+          main: 'entry.html',
         },
       },
     },
@@ -23,7 +22,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve('.'),
+        '@': '/src',
       }
     }
   };
